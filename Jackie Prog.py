13cb6965e3a9f7d2619ed01477ef1f24bcf3a636
heatonj <heatonj@stu.otc.school.nz>
2021-08-17 15:10:34 +1200

    Initial commit to GitHub 17/08

--- Jackie Prog.py
+++ Jackie Prog.py
@@ -1,1 +1,15 @@
-#Jackie
+#Find the gender
+gender_list = ["Boy", "Girl", "Unisex"]
+
+def gender():
+    print(gender_list)
+user_gender = input("What gender would you like?")
+if user_gender in gender_list:
+    print("The gender you picked is {}".format(user_gender))
+else:
+    print("Your previous input is not available")
+    print("These are the options available {}".format(gender_list))
+gender()
+
+
+
