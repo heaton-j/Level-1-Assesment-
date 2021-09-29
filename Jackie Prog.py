24c968bf83e6c17f9769752c54ca09661e116a3f
heatonj <heatonj@stu.otc.school.nz>
2021-09-30 09:36:01 +1300

    Need to fix the input problem, if you put in the wrong input first then the right later it still comes up as none and prints no names or definitions. Deleted the spaces bwteeen all of the names lists and definitions lists.

--- Jackie Prog.py
+++ Jackie Prog.py
@@ -1,4 +1,5 @@
-# Find the gender
+# Find the gender the user wants for their names
+
 gender_list = ["Boy", "Girl", "Unisex"]
 
 def gender():
@@ -8,11 +9,12 @@ def gender():
         print("The gender you picked is {}".format(user_gender))
         return user_gender
     else:
-         print("Your previous input is not available, These are the options available {}".format(gender_list))
+         print("Your previous input is not available")
          gender()
 User_Gender = gender()
 
-# Find type of name
+# Find type of name the user wants for their name
+
 category_list = ["Popular", "Traditional", "Unique"]
 
 def category():
@@ -22,48 +24,31 @@ def category():
         return user_category
     else:
         print("Your previous input is not available")
-        print("these are the options available {}".format(category_list))
         category()
 User_Category = category()
 
 # List all possible names
 
 boy_unique = ["Felix", "Jasper", "Kenji", "Asnee"]
-
 boy_traditional = ["Arthur", "James", "William", "Fredrick", "Vincent"]
-
 boy_popular = ["Jack","Lucas", "Oliver", "Liam", "Michael"]
-
 girl_unique = ["Annalise", "Genevieve", "Willow", "Hana", "Mei", "Iris"]
-
 girl_traditional = ["Elizabeth", "Victoria","Charlotte", "Marie", "Audrey"]
-
 girl_popular = ["Ava", "Emma", "Sarah", "Evelyn", "Bella", "Luna"]
-
 unisex_unique = ["Aspen",  "Phoenix", "Lumi", "Nico","Armani"]
-
 unisex_traditional = ["Hura", "Dana", "Kennedy"]
-
 unisex_popular = ["Jamie", "Alex", "Sam", "Freddie", "Riley"]
 
-# list all possible definitions
+# list all possible definitions to the names
 
 boy_unique_definitions = ["Happy", "Treasurer", "Strong", "Lightning"]
-
 boy_traditional_definitions = ["Bear or Thor,the eagle", "Supplanter","Resolute protection", "Peaceful ruler", "Conquer"]
-                                                                                
 boy_popular_definitions = ["Supplanter", "Bright, Shining", "Peaceful","Strong-willed warrior", "Who is like God?"]
-
 girl_unique_definitions = ["Graced with God's bounty", "Woman of the race","Freedom", "Flower", "Beautiful", "Rainbow"]
-
 girl_traditional_definitions = ["Gift of God", "Victory", "Free", "Star of the sea", "Noble strength"]
-
 girl_popular_definitions = ["Life", "Life", "Princess", "Wished for child", "Beautiful", "Moon"]
-
 unisex_unique_definitions = ["Quaking tree", "Blood red","Snow", "People of victory", "Warrior"]
-
 unisex_traditional_definitions = ["Misshapen head", "Generosity", "Free woman"]
-
 unisex_popular_definitions = ["Supplanter", "Warrior", "Told by God", "Peace Ruler", "Valiant"]
 
 # print out final names for user to see
@@ -106,25 +91,25 @@ Definition = definitions()
 # print out the definitions if user wants to access them
 
 def final_definitions(definition, user_gender, user_category):
-    if user_gender == "Girl":
-        if user_category == "Unique":
+    if User_Gender == "Girl":
+        if User_Category == "Unique":
             print(girl_unique_definitions)
-        elif user_category == "Popular":
+        elif User_Category == "Popular":
             print(girl_popular_definitions)
-        elif user_category == "Traditional":
+        elif User_Category == "Traditional":
             print(girl_traditional_definitions)
-    if user_gender == "Boy":
-        if user_category == "Unique":
+    if User_Gender == "Boy":
+        if User_Category == "Unique":
             print(boy_unique_definitions)
-        elif user_category == "Popular":
+        elif User_Category == "Popular":
             print(boy_popular_definitions)
-        elif user_category == "Traditional":
+        elif User_Category == "Traditional":
             print(boy_traditional_definitions)
-    if user_gender == "Unisex":
-        if user_category == "Unique":
+    if User_Gender == "Unisex":
+        if User_Category == "Unique":
             print(unisex_unique_definitions)
-        elif user_category == "Popular":
+        elif User_Category == "Popular":
             print(unisex_popular_definitions)
-        elif user_category == "Traditional":
+        elif User_Category == "Traditional":
             print(unisex_traditional_definitions)
 final_definitions(Definition, User_Gender, User_Category)
